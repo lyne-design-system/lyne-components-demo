37110e5854da10c48ef216fb37d8e3bf9d824a0b
Yves Tscherry <info@feerglas.com>
2021-10-28 09:29:53 +0200

    chore: remove sample content

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>Hello Nuxt 3!</h1>
-    <a href="https://v3.nuxtjs.org" target="_blank" rel="noopener">Read the docs</a>
+    <h1>lyne-components-demo</h1>
   </div>
 </template>
