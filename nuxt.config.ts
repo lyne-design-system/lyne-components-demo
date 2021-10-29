a6504142c78a0a383d5e9a13ffa5edde6d0cdeb5
Yves Tscherry <info@feerglas.com>
2021-10-29 13:57:09 +0200

    chore: try hooks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,32 @@
 import { defineNuxtConfig } from 'nuxt3';
 
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  build: {
+    transpile: ['lyne-test']
+  },
+
+  hooks: {
+    'build:done': () => {
+      console.log('<<<<<<<<<<<<<<<<<<<<<< xxxxxx hooks build done');
+    },
+    'render:done': () => {
+      console.log('<<<<<<<<<<<<<<<<<<<<<< xxxxxx hooks render  done');
+    },
+    'render:route': () => {
+      console.log('<<<<<<<<<<<<<<<<<<<<<< xxxxxx hooks render  route');
+    }
+  },
+
+  modules: [
+   // '~/modules/hooks'
+  ],
+  // dev: false,
+  // vite: false,
+  // target: 'static',
+  // ssr: false,
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag: string) => ['lyne-button'].includes(tag)
+    }
+  }
+});
