7daef7ae5eff26610a423caae6e2dd73891ac82d
Yves Tscherry <info@feerglas.com>
2021-11-01 15:15:49 +0100

    chore: add page to stresstest components usage

---
+++ pages/stresstest.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div
+      v-for="(component, index) in $data.components"
+      :key="index"
+    >
+      <div
+        v-for="item in $data.items"
+        :key="item"
+      >
+        <p>{{component}} {{item}}</p>
+        <component v-bind:is="component"></component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      components: [
+        'Accordion',
+        'Button',
+        'Clock',
+        'Heroteaser',
+        'ImageWrapper',
+        'Logo',
+        'Panel',
+        'Pearlchain',
+        'Signet',
+        'Title'
+      ],
+      items: 50
+    };
+  }
+}
+</script>
