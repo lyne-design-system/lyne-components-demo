35e8f0d78071b8765ab4d2e6351d52739546a4f2
Yves Tscherry <info@feerglas.com>
2021-11-01 15:36:03 +0100

    fix: use generate:page hook instead of render:route

--- modules/stencil-render.js
+++ modules/stencil-render.js
@@ -2,12 +2,15 @@ const hydrate = require('lyne-test/hydrate');
 
 export default function StencilRenderModule() {
 
-  this.nuxt.hook('render:route', async (url, page) => {
+  this.nuxt.hook('generate:page', async (page) => {
+
     const _page = page;
+
     const render = await hydrate.renderToString(_page.html, {
       clientHydrateAnnotations: true,
       removeScripts: false,
-      removeUnusedStyles: false
+      removeUnusedStyles: false,
+      url: _page.path
     });
 
     _page.html = render.html;
