87a12fd7658bccde5c9c5ce1c932eccbafe5d13b
Yves Tscherry <info@feerglas.com>
2021-11-01 14:58:35 +0100

    chore: add wrapper component for hero teaser

---
+++ components/Heroteaser.vue
@@ -0,0 +1,3 @@
+<template>
+  <lyne-teaser-hero button-text="Button text" image-loading="eager" image-src="https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg" link="https://www.sbb.ch" new-window-info-text="" open-in-new-window="false" text="Panel text"></lyne-teaser-hero>
+</template>
