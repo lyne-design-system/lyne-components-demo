129c0e42ce23c35be5648fdb733a58d69f3a8b49
Yves Tscherry <info@feerglas.com>
2021-11-01 11:15:20 +0100

    fix: fix linting issues

--- modules/stencil-render.js
+++ modules/stencil-render.js
@@ -1,15 +1,20 @@
-const hydrate = require('lyne-test/hydrate')
+const hydrate = require('lyne-test/hydrate');
 
 export default function StencilRenderModule() {
 
-    this.nuxt.hook('render:route', async (url, page) => {
-        const render = await hydrate.renderToString(page.html, {
-            clientHydrateAnnotations: true,
-            removeScripts: false,
-            removeUnusedStyles: false,
-        });
-    
-        page.html = render.html;
+  this.nuxt.hook('render:route', async (url, page) => {
+    const _page = page;
+    const render = await hydrate.renderToString(_page.html, {
+      clientHydrateAnnotations: true,
+      removeScripts: false,
+      removeUnusedStyles: false
     });
-    
-  }
+
+    _page.html = render.html;
+
+    /* eslint-disable no-param-reassign */
+    page = _page;
+    /* eslint-enable no-param-reassign */
+  });
+
+}
--- plugins/define-custom-elements.js
+++ plugins/define-custom-elements.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 
-Vue.config.ignoredElements = [new RegExp(`lyne-\w*`)];
+Vue.config.ignoredElements = [new RegExp('lyne-*', 'u')];
 
 export default () => {
   if (process.client) {
-    const { defineCustomElements } = require('lyne-test/dist/esm/loader');
+    const {
+      defineCustomElements
+    } = require('lyne-test/dist/esm/loader');
 
     defineCustomElements(window);
 
