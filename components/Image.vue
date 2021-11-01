d09f61149a15283a8a875bdd8de4d0d5dd9daf88
Yves Tscherry <info@feerglas.com>
2021-11-01 14:59:10 +0100

    chore: add wrapper component for image

---
+++ components/Image.vue
@@ -0,0 +1,3 @@
+<template>
+  <lyne-image alt="" caption="Mit Ihrem Halbtax profitieren Sie zudem von attraktiven Zusatzleistungen und Rabatten. Wenn Sie unter 25 Jahre jung sind, können Sie zu Ihrem Halbtax das beliebte <a href=&quot;https://www.sbb.ch/abos-billette/abonnemente/gleis-7-freie-fahrt-ab-19-uhr.html#jahrg_nger_halbtax&quot;>Gleis 7</a> dazu kaufen." copyright="" copyright-holder="Organization" custom-focal-point="false" focal-point-debug="false" focal-point-x="" focal-point-y="" hide-from-screenreader="false" image-src="" image-src-examples="https://cdn.img.sbb.ch/content/dam/internet/lyne/Billetkontrolle.jpg" loading="eager" lqip="true" performance-mark=""></lyne-image>
+</template>
