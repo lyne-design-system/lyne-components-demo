35a6c563cf2c06aaad6bbb12495be5e0ab9e66b4
Yves Tscherry <info@feerglas.com>
2021-11-01 10:17:57 +0100

    feat: add sample page with lyne-button

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <lyne-button
+    variant="primary"
+    size="large"
+    label="Button"
+    name="sample-name"
+    value="sample-value"
+  ></lyne-button>
+</template>
+
+<script>
+export default {}
+</script>
