7d2e8aaf69d0a265110606e8c3d9a4b990dda1d8
Yves Tscherry <info@feerglas.com>
2021-11-01 14:31:19 +0100

    chore: add test page

---
+++ pages/kitchensink.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Lyne Components Demo Kitchensink</h1>
+
+    <NuxtLink to="/">Home</NuxtLink>
+
+    <lyne-button
+      variant="primary"
+      size="large"
+      label="Button"
+      name="sample-name"
+      value="sample-value"
+    ></lyne-button>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
