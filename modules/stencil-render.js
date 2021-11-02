4882780d77dbcede97c330425ed374a1d8c2737b
Yves Tscherry <info@feerglas.com>
2021-11-02 13:12:35 +0100

    fix: add render:route hook to hydrate on dev build

--- modules/stencil-render.js
+++ modules/stencil-render.js
@@ -3,23 +3,30 @@ const hydrate = require('lyne-test/hydrate');
 export default function StencilRenderModule() {
 
   /**
-   * We do server-side hydration only for the production build.
-   * During development, there is no server-side hydration.
-   *
-   * To check your work before releasing, do the following:
-   * `npm run generate`
-   * `npm start`
+   * Server-side hydration for the production build.
    */
   this.nuxt.hook('generate:page', async (page) => {
+    const _page = page;
+
+    const render = await hydrate.renderToString(_page.html, {
+      removeHtmlComments: true
+    });
+
+    _page.html = render.html;
+  });
+
+  /**
+   * Server-side hydration for the dev build. Since we use static target
+   * from nuxt, `render:route` hook should not get called on netlify,
+   * which is a static host.
+   */
+  this.nuxt.hook('render:route', async (url, page) => {
+
     const _page = page;
 
     const render = await hydrate.renderToString(_page.html);
 
     _page.html = render.html;
 
-    /* eslint-disable no-param-reassign */
-    page = _page;
-    /* eslint-enable no-param-reassign */
   });
-
 }
